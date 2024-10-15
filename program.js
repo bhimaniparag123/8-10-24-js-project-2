e9a0f3a079098176fea7f4a1e9ed6e23c7aebeab
bhimaniparag123 <159759757+bhimaniparag123@users.noreply.github.com>
2024-10-15 11:25:57 +0530

    Add files via upload

---
+++ program.js
@@ -0,0 +1,48 @@
+// #1
+// let number = parseInt(prompt("Enter a number:"));
+// let result = (number % 2 === 0) ? "Even" : "Odd";
+
+// console.log(`${number} is ${result}.`);
+
+
+
+// #2
+// let temperature = parseFloat(prompt("Enter the temperature:"));
+// let result = (temperature < 0) ? "You temperature Freezing" :
+//              (temperature >= 0 && temperature <= 20) ? "You temperature Cool" :
+//              (temperature > 20) ? "You temperature Warm" :
+//              "Invalid temperature input";
+
+// console.log(result);
+
+
+
+//#3
+// let score = parseFloat(prompt("Enter the score (0 to 100):"));
+// let result = (score < 0 || score > 100) ? "Invalid score" :
+//              (score >= 90 && score < 100) ? "Grade: A" :
+//              (score >= 80 && score < 90) ? "Grade: B" :
+//              (score >= 70 && score < 80) ? "Grade: C" :
+//              (score >= 60 && score < 70) ? "Grade: D" :
+//              (score >= 0 && score < 60) ? "Grade: E" :
+//              "Invalid input";
+// console.log(result);
+
+
+
+//#4
+// let age = parseInt(prompt("Enter the age:"));
+// let result = (age < 0) ? "Invalid Age" :
+//              (age >= 0 && age <= 12) ? "You Are Child" :
+//              (age >= 13 && age <= 19) ? "You Are Teenager" :
+//              (age >= 20 && age <= 64) ? "You Are Adult" :
+//              (age > 64) ? "You Are Senior" : 
+//              "Age does not fail under any classification";
+
+// console.log(result);
+
+
+//5
+// let count = parseInt(prompt("Enter the count:"));
+// let result = (count > 100) ? "Applied" : "Not Applied";
+// console.log(result);
